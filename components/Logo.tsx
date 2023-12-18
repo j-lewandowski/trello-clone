ff9e34efcb38d17d12ea4f86a7f6607bcc92bbcd
Jakub Lewandowski <kuba@Jakubs-MacBook-Air.local>
2023-12-18 20:40:00 +0100

    fixed clerk issues

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -8,19 +8,23 @@ const headingFont = localFont({
   src: "../public/fonts/font.woff2",
 });
 
-const Logo = () => {
+export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          height={30}
+          width={30}
+        />
+        <p className={cn(
+          "text-lg text-neutral-700 pb-1",
+          headingFont.className,
+        )}>
           Taskify
         </p>
       </div>
     </Link>
   );
 };
-
-export default Logo;
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
   name: "Taskify",
-  description: "Colaborate, manage projects, and reach new productivity peaks",
+  description: "Collaborate, manage projects, and reach new productivity peaks",
 };
