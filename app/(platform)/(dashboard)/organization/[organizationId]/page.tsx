827266b1e1823b4867bffb7124cc614a66e9b60e
Jakub Lewandowski <kubalew.pl@gmail.com>
2023-12-27 18:25:56 +0100

    handled organizations with clerk

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,10 @@
+import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+
 const OrganizationIdPage = () => {
+  const { userId, orgId } = auth();
   return (
     <div>
-      Organization Page
+      <OrganizationSwitcher />
     </div>
   );
 };
